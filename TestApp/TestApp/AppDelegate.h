0428436827998b5cd0e84d014003a264c8ece767
Tobias <tobias.ogemark@wirelesscar.com>
2015-12-23 10:58:12 -0500

    First version

---
+++ TestApp/TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Tobias Ogemark on 12/23/15.
+//  Copyright © 2015 Tobias Ogemark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Tobias Ogemark on 12/23/15.
+//  Copyright © 2015 Tobias Ogemark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
